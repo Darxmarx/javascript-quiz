53e0907b4d0e1d3a896ed10d58f484f58d2630d1
Darx <mcrivabode@gmail.com>
2022-10-03 15:30:58 -0700

    implement questions and respective answers into variable

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+var quizStartBtn = document.querySelector("#quiz-start-btn");
+
+//all question contents stored here
+var quizContents = [
+    {
+        question: "Commonly used data types do NOT include:"
+        answer: "booleans"
+        options: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+            ]
+    },
+    {
+        question: "Commonly used data types do NOT include:"
+        answer: "booleans"
+        options: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+            ]
+    },
+    {
+        question: "Commonly used data types do NOT include:"
+        answer: "booleans"
+        options: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+            ]
+    },
+    {
+        question: "Commonly used data types do NOT include:"
+        answer: "booleans"
+        options: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+            ]
+    },
+    {
+        question: "Commonly used data types do NOT include:"
+        answer: "booleans"
+        options: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+            ]
+    }
+];
+
+function quizStart() {
+    var secondsLeft = 120; //start quiz with 120 seconds
+
+
+}
+
+
+
+
+
+
+
+
+
+
+//upon clicking start quiz button, the quiz begins
+quizStartBtn.addEventListener("click", quizStart);
